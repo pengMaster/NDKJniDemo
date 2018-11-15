fa9b9ba4e0a67a2aadd7d730fb8400ee9ab89e26
wp521 <946549990@qq.com>
2018-11-15 17:25:32 +0800

    modify_read_me

--- app/src/main/jni/JNIControl.cpp
+++ app/src/main/jni/JNIControl.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "king_bird_ndkjnidemo_JNIUtils.h"
+//king_bird_ndkjnidemo_JNIUtils_printStringByJni 包名+文件名+文件内方法名
 JNIEXPORT jstring JNICALL Java_king_bird_ndkjnidemo_JNIUtils_printStringByJni
         (JNIEnv *env, jclass jclass){
+    //字符串返回
 return env->NewStringUTF("没想到吧！我竟然会JNI了！！！");
 }
